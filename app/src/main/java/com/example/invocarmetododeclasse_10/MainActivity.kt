460b4279b315b9955fe9d2c4fbbf9ac0e5d8dc9c
Gilmarcos Santos <GILMARCOSXSANTOS@GMAIL.COM>
2022-12-30 12:31:12 -0300

    Salvar

--- app/src/main/java/com/example/invocarmetododeclasse_10/MainActivity.kt
+++ app/src/main/java/com/example/invocarmetododeclasse_10/MainActivity.kt
@@ -7,27 +7,43 @@ import android.util.Log
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    /* Declaração de Variávevl em Escopo Global: */
+    private lateinit var txtViewUm: TextView
+    private lateinit var txtViewDois: TextView
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /* Declarar Funções: */
+        declararVariaveis()
+
         val somar001 = ClasseSomar001()
         val resultado001 = somar001.funcaoSomar001(99)
         Log.d("ResultadoAAA ", resultado001.toString())
-//        var minhaTextView001 = findViewById<TextView>(R.id.minhaTextView)
-//        minhaTextView001.text = "ResultadoAAA = " + resultado001.toString()
+//        txtViewUm.text = "ResultadoAAA = $resultado001"
 
         val somar002 = ClasseSomar002()
         val resultado002 = somar002.funcaoSomar002(150, 75)
         Log.d("ResultadoAAB ", resultado002.toString())
-//        var minhaTextView002 = findViewById<TextView>(R.id.minhaTextView)
-//        minhaTextView002.text = "ResultadoAAB = $resultado002"
+//        txtViewUm.text = "ResultadoAAB = $resultado002"
 
         val somar003 = ClasseSomar003()
         val resultado003 = somar003.funcaoSomar003(501)
         Log.d("ResultadoAAC ", resultado003.toString())
-        var minhaTextView003 = findViewById<TextView>(R.id.minhaTextViewUm)
-        minhaTextView003.text = "ResultadoAAC = $resultado003"
+        // txtViewUm.text = "ResultadoAAC = $resultado003"
+
+//        val somar004 = ClasseSomar004()
+//        val resultado004 = somar004.funcaoSomar004(501)
+//        Log.d("ResultadoAAD ", resultado004.toString())
+//        txtViewUm.text = "ResultadoAAD = $resultado004"
     }
+
+    private fun declararVariaveis() {
+        txtViewUm = findViewById(R.id.minhaTextViewUm)
+        txtViewDois = findViewById(R.id.minhaTextViewDois)
+    }
+
 }
